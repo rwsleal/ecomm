e967db480a43d32f537eaa71eb32c4027b9a3855
rwsleal <rwsleal@gmail.com>
2023-01-04 18:08:36 -0300

    feat: create a function to save the new user on the simulated database

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -1,9 +1,16 @@
 import * as fs from 'fs';
 
 const getUserId = () => {
-    const accounts = JSON.parse(fs.readFileSync('accounts.json'))
+    const accounts = JSON.parse(fs.readFileSync('accounts.json'));
 
     return accounts.length + 1
+}
+
+const saveUser = (user) => {
+    const accounts = JSON.parse(fs.readFileSync('accounts.json'));
+    const newAccounts = [...accounts, user];
+
+    fs.writeFileSync('accounts.json', JSON.stringify(newAccounts));
 }
 
 const createUserUseCase = (name, email, password) => {
@@ -15,7 +22,11 @@ const createUserUseCase = (name, email, password) => {
         createdDate: new Date().toISOString().substring(0,10)
     }
     
+    saveUser(userInfo);
+
     return userInfo
 }
+
+console.log(createUserUseCase('nome', 'email', 'password'));
 
 export { createUserUseCase }
