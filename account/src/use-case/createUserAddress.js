02fc32d3f58d5a26424e5d9e109d9262297f8899
rwsleal <rwsleal@gmail.com>
2023-01-05 15:13:53 -0300

    feat: implements function createUserAddressUseCase

---
+++ account/src/use-case/createUserAddress.js
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+
+const accounts = JSON.parse(fs.readFileSync('./src/use-case/accounts.json'));
+
+const createUserAddressUseCase = (email, address) => {
+    const userToCreateAddress = accounts.find((user) => email === user.email);
+
+    if (userToCreateAddress) {
+        const userIndex = accounts.indexOf(userToCreateAddress);
+
+        const userWithAddress = { ...userToCreateAddress, address };
+
+        accounts.splice(userIndex, 1, userWithAddress);
+
+        fs.writeFileSync('./src/use-case/accounts.json', JSON.stringify(accounts));
+
+        return accounts.some((user) => user.email === email && user.address === address);
+    }
+
+    return false
+}
+
+export { createUserAddressUseCase }
