c1017e74a74cc255584d8773d231be75b53a694e
rwsleal <rwsleal@gmail.com>
2023-01-04 21:15:26 -0300

    fix: import paths

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -1,13 +1,13 @@
 import * as fs from 'fs';
 
 const getUserId = () => {
-    const accounts = JSON.parse(fs.readFileSync('accounts.json'));
+    const accounts = JSON.parse(fs.readFileSync('./src/use-case/accounts.json')); //precisei colocar o caminho relativo ao root para o container funcionar corretamente
 
     return accounts.length + 1
 }
 
 const saveUser = (user) => {
-    const accounts = JSON.parse(fs.readFileSync('accounts.json'));
+    const accounts = JSON.parse(fs.readFileSync('./src/use-case/accounts.json'));
     const newAccounts = [...accounts, user];
 
     fs.writeFileSync('accounts.json', JSON.stringify(newAccounts));
--- account/test/createUserAccount.test.js
+++ account/test/createUserAccount.test.js
@@ -1,4 +1,4 @@
-import { createUserUseCase } from "../src/use-case/createUserAccount";
+import { createUserUseCase } from "../src/use-case/createUserAccount.js";
 
 const response = createUserUseCase('Josué Lima', 'josuelima@email.com', 'senhaDoJosue');
 
