f54fd542488ae315df16ba15ce21f9c809290e0c
rwsleal <rwsleal@gmail.com>
2023-01-05 00:59:57 -0300

    feat: implements function removeUserUseCase

--- account/src/use-case/removeUserAccount.js
+++ account/src/use-case/removeUserAccount.js
@@ -1,16 +1,21 @@
 import * as fs from 'fs';
-import { searchUserAccountByEmailUseCase } from './searchUserAccountByEmail';
 
 const accounts = JSON.parse(fs.readFileSync('./src/use-case/accounts.json'));
 
 const removeUserUseCase = (email) => {
-    const userToBeRemoved = searchUserAccountByEmailUseCase(email);
+    const userToBeRemoved = accounts.find((user) => email === user.email);
 
     if (userToBeRemoved) {
         const userIndex = accounts.indexOf(userToBeRemoved);
 
         accounts.splice(userIndex, 1);
+
+        fs.writeFileSync('./src/use-case/accounts.json', JSON.stringify(accounts));
+
+        return !accounts.includes(userToBeRemoved);
     }
 
-    return accounts.includes(userToBeRemoved);
-} 
+    return false
+}
+
+export { removeUserUseCase };
