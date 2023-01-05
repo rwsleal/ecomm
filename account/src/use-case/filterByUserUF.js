3afd8d66e35d413e38b1d6dd263525b9dc067d51
rwsleal <rwsleal@gmail.com>
2023-01-05 16:43:08 -0300

    feat: implements function filterByUserUFUseCase

---
+++ account/src/use-case/filterByUserUF.js
@@ -0,0 +1,13 @@
+import * as fs from 'fs';
+
+const accounts = JSON.parse(fs.readFileSync('./src/use-case/accounts.json'));
+
+const filterByUserUFUseCase = (uf) => {
+    const filteredByUF = accounts.filter(({ address }) => address?.uf == uf);
+
+    if (!filteredByUF.length) return false;
+
+    return filteredByUF;
+}
+
+export { filterByUserUFUseCase }
