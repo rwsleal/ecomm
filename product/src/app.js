f1b0aa52b8c0c0e8386bbaa7c389a30123331dd8
rwsleal <rwsleal@gmail.com>
2023-02-03 23:00:32 -0300

    feat: implements express error handler

--- product/src/app.js
+++ product/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import db from './database/config/mongodbConnection.js';
 import { categoriesRoutes } from './routes/index.js';
+import { errorHandler } from './middlewares/index.js';
 
 db.once('open', () => {
     console.log('MongoDB succesfully connected!');
@@ -11,5 +12,6 @@ const app = express();
 app.use(express.json());
 
 app.use(categoriesRoutes);
+app.use(errorHandler);
 
 export default app;
---
+++ product/src/middlewares/errorHandler.js
@@ -0,0 +1,10 @@
+const errorHandler = (err, req, res, _next) => {
+    if (err.message.includes('|')) {
+        const [code, message] = err.message.split('|');
+        return res.status(Number(code)).json({ message });
+    }
+
+    return res.status(500).json({ message: 'Something went wrong' });
+};
+
+export default errorHandler;
