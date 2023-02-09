9d4ab7235f4fcbdc6a7361f5c32003019a0a2fde
rwsleal <rwsleal@gmail.com>
2023-02-09 20:54:29 -0300

    feat: implements generic handlers (errors and validations)

---
+++ finance/src/middlewares/errorHandler.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, _next) => {
+    console.log(err);
+
+    if (err.message.includes('|')) {
+        const [code, message] = err.message.split('|');
+        return res.status(Number(code)).json({ message });
+    }
+
+    return res.status(500).json({ message: 'Something went wrong' });
+};
+
+module.exports = errorHandler;
