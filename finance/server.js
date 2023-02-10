95f63909f8ac912cc99b9a383a1df1f343a6d16c
rwsleal <rwsleal@gmail.com>
2023-02-10 16:20:39 -0300

    fix: payments server port

--- finance/server.js
+++ finance/server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app.js');
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3004;
 
 app.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`);
