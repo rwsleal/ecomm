6a8c55a6a7c63944504c784ba0420219fa5164ca
rwsleal <rwsleal@gmail.com>
2023-02-10 16:21:19 -0300

    feat: implements payments endpoints

--- finance/src/app.js
+++ finance/src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { errorHandler } = require('./middlewares/index.js');
+const paymentsRoutes = require('./routes/index.js');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(paymentsRoutes);
+app.use(errorHandler);
 
 module.exports = app;
