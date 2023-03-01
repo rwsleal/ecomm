eeecbf6b1ec21e1af575170aeb09147d8a7a5ea3
rwsleal <rwsleal@gmail.com>
2023-03-01 09:39:47 -0300

    test: implements route GET /products

--- product/test/routes/productsRoutes.test.js
+++ product/test/routes/productsRoutes.test.js
@@ -7,28 +7,44 @@ import Product from '../../src/models/Product.js';
 import { allProductsMock } from '../../mocks/productsMocks.js';
 
 describe('GET /products', () => {
-    beforeEach(() => {
-        mockingoose(Product).toReturn(allProductsMock, 'find');
+    describe('Case it succeceds', () => {
+        beforeEach(() => {
+            mockingoose(Product).toReturn(allProductsMock, 'find');
+        });
+    
+        afterEach(() => {
+            mockingoose.resetAll();
+        });
+
+        it('should return an array with all products', async () => {
+            const response = await request(app)
+                .get('/products')
+                .set('Accept', 'application/json')
+                .expect('content-type', /json/)
+                .expect(200);
+            
+            expect(response.body).toStrictEqual(allProductsMock);
+        });
     });
 
-    afterEach(() => {
-        mockingoose.resetAll();
+    describe('Case it does not find any product', () => {
+        beforeEach(() => {
+            mockingoose(Product).toReturn([], 'find');
+        });
+    
+        afterEach(() => {
+            mockingoose.resetAll();
+        });
+
+        it('should return an empty array', async () => {
+            const response = await request(app)
+                .get('/products')
+                .set('Accept', 'application/json')
+                .expect('content-type', /json/)
+                .expect(200);
+            
+            expect(typeof (response.body)).toBe('object');
+            expect(response.body).toHaveLength(0);
+        });
     });
-
-    it('should return an array with all products', async () => {
-        const response = await request(app)
-            .get('/products')
-            .set('Accept', 'application/json')
-            .expect('content-type', /json/)
-            .expect(200);
-        
-        expect(response.body).toStrictEqual(allProductsMock);
-    });
-
-    // it('should return 200 as status code', async () => {
-    //     const response = await request(app)
-    //         .get('/products')
-
-    //     expect(response.status).toEqual(200);
-    // });
 });
