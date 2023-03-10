0c39e42e01bd7027d4aa2e732f95599196a5e796
rwsleal <rwsleal@gmail.com>
2023-03-10 19:05:52 -0300

    fix: use crypto to generate a token hash instead of bcryptjs

--- account/redis/blacklistManagement.js
+++ account/redis/blacklistManagement.js
@@ -1,20 +1,24 @@
 import { promisify } from 'util';
+import { createHash } from 'crypto';
 import blacklist from './blacklist.js';
 import { decodeTokenExp } from '../src/helpers/jwtHelper.js';
-import { createHash } from '../src/helpers/bcryptHelper.js';
 
 const existsAsync = promisify(blacklist.exists).bind(blacklist);
 const setAsync = promisify(blacklist.set).bind(blacklist);
 
+const generateTokenHash = (token) => createHash('sha256')
+  .update(token)
+  .digest('hex');
+
 const blacklistAdd = async (token) => {
-    const tokenHash = createHash(token);
+    const tokenHash = generateTokenHash(token);
     const expirationDate = decodeTokenExp(token);
     await setAsync(tokenHash, '');
     blacklist.expireat(tokenHash, expirationDate);
 };
 
 const blacklistCheck = async (token) => {
-    const tokenHash = createHash(token);
+    const tokenHash = await generateTokenHash(token);
     const result = await existsAsync(tokenHash);
     return result === 1;
 };
