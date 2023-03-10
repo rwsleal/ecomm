96e0916c6b6871c236f795b3a84779e13563fe2b
rwsleal <rwsleal@gmail.com>
2023-03-10 09:08:30 -0300

    feat: implements bearer strategy

--- account/src/middlewares/authStrategies.js
+++ account/src/middlewares/authStrategies.js
@@ -1,7 +1,9 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as BearerStrategy } from 'passport-http-bearer';
 import Account from '../models/Account.js';
 import { checkPassword } from '../helpers/bcryptHelper.js';
+import { checkToken } from '../helpers/jwtHelper.js';
 
 passport.use(
     new LocalStrategy({
@@ -14,19 +16,33 @@ passport.use(
                 return done(err);
             }
             if (account === null) {
-                return done(new Error('401|Invalid email provided'));
+                return done(new Error('401|Invalid credentials provided'));
             }
 
             const check = checkPassword(password, account.password);
-            
+
             if (!check) {
                 console.log(check);
-                return done(new Error('401|Invalid password provided'));
+                return done(new Error('401|Invalid credentials provided'));
             }
 
             return done(null, account);
         });
     }),
+);
+
+passport.use(
+    new BearerStrategy(
+        async (token, done) => {
+            try {
+                const payload = checkToken(token);
+                const account = await Account.findOne({ id: payload.id });
+                done(null, account, { token });
+            } catch (err) {
+                done(err);
+            }
+        },
+    ),
 );
 
 // eslint-disable-next-line import/prefer-default-export
