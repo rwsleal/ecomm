9e48b2ded734283973036f36365a79259e6d487d
rwsleal <rwsleal@gmail.com>
2023-03-10 10:56:01 -0300

    feat: implements bearer strategy to product API routes

--- product/src/routes/categoriesRoutes.js
+++ product/src/routes/categoriesRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import passport from 'passport';
 import { categoriesController } from '../controllers/index.js';
 import { validationHandler } from '../middlewares/index.js';
 import { categorySchema } from '../joiSchemas/index.js';
@@ -6,13 +7,35 @@ import { categorySchema } from '../joiSchemas/index.js';
 import 'express-async-errors';
 
 const router = express.Router();
+const bearerAuthenticate = passport.authenticate('bearer', { session: false });
+
 const adminEndpointById = '/admin/categories/:id';
 
 router
-    .post('/admin/categories', validationHandler(categorySchema), categoriesController.create)
-    .get('/categories/:id', categoriesController.getById)
-    .put(adminEndpointById, categoriesController.update)
-    .patch(adminEndpointById, categoriesController.update)
-    .delete(adminEndpointById, categoriesController.remove);
+    .post(
+        '/admin/categories',
+        bearerAuthenticate,
+        validationHandler(categorySchema),
+        categoriesController.create,
+        )
+    .get(
+        '/categories/:id',
+        categoriesController.getById,
+        )
+    .put(
+        adminEndpointById,
+        bearerAuthenticate,
+        categoriesController.update,
+        )
+    .patch(
+        adminEndpointById,
+        bearerAuthenticate,
+        categoriesController.update,
+        )
+    .delete(
+        adminEndpointById,
+        bearerAuthenticate,
+        categoriesController.remove,
+        );
 
 export default router;
--- product/src/routes/productsRoutes.js
+++ product/src/routes/productsRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import passport from 'passport';
 import { productsController } from '../controllers/index.js';
 import { validationHandler, categoryCheck } from '../middlewares/index.js';
 import { productSchema } from '../joiSchemas/index.js';
@@ -6,15 +7,30 @@ import { productSchema } from '../joiSchemas/index.js';
 import 'express-async-errors';
 
 const router = express.Router();
-// const adminEndpointById = '/admin/categories/:id';
+const bearerAuthenticate = passport.authenticate('bearer', { session: false });
 
 router
-    .get('/products', productsController.getAll)
-    .get('/products/:id', productsController.getById)
-    .put('/admin/products/:id', productsController.update)
-    .delete('/admin/products/:id', productsController.remove)
+    .get(
+        '/products',
+        productsController.getAll,
+        )
+    .get(
+        '/products/:id',
+        productsController.getById,
+        )
+    .put(
+        '/admin/products/:id',
+        bearerAuthenticate,
+        productsController.update,
+        )
+    .delete(
+        '/admin/products/:id',
+        bearerAuthenticate,
+        productsController.remove,
+        )
     .post(
         '/admin/products',
+        bearerAuthenticate,
         validationHandler(productSchema),
         categoryCheck,
         productsController.create,
